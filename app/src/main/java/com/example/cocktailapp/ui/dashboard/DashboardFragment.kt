937deda848eccd1b13a554f32c9faa0a33872937
Bunsarak PEN <bunsarak.pen@ynov.com>
2021-04-01 15:42:27 +0200

    CocktailApp Dashboard :
    - Add Gridview and Cardview
    - Get number of cocktail
    - Header background of DashboardFragment.kt
    - Icon cocktail
    - add binding and splitties to DashboardFragment.kt

--- app/src/main/java/com/example/cocktailapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/cocktailapp/ui/dashboard/DashboardFragment.kt
@@ -1,31 +1,85 @@
 package com.example.cocktailapp.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.cocktailapp.CocktailList
+import com.example.cocktailapp.CoktailService
 import com.example.cocktailapp.R
+import com.example.cocktailapp.RecyclerAdapter
+import com.example.cocktailapp.databinding.FragmentDashboardBinding
+import com.google.android.material.snackbar.Snackbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import splitties.alertdialog.material.materialAlertDialog
+import splitties.toast.toast
+
+
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentDashboardBinding.inflate(layoutInflater)
         dashboardViewModel =
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        val textView: String = getString(R.string.title_dashboard)
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            textView
         })
-        return root
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.thecocktaildb.com/api/json/v1/1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val service = retrofit.create(CoktailService::class.java)
+        val call = service.getCocktailData()
+
+        call.enqueue(object : Callback<CocktailList> {
+            override fun onResponse(call: Call<CocktailList>, response: Response<CocktailList>) {
+                if (response.code() == 200) {
+                    println("cocktail number DASHBOARD" + " " + response.body().cocktails.size)
+                }
+            }
+            override fun onFailure(call: Call<CocktailList>, t: Throwable) {
+                error(t.message.toString())
+            }
+        })
+
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        binding.cardDashboardCocktail.setOnClickListener{
+            toast("Hello WORLD")
+        }
+
+
+
     }
 }
