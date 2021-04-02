38768da811cd5caf97f0d91359cdab0f5dafc1f7
gpatruno <56818714+gpatruno@users.noreply.github.com>
2021-04-02 16:38:47 +0200

    Add Database

--- app/src/main/java/com/example/cocktailapp/CocktailAdapter.kt
+++ app/src/main/java/com/example/cocktailapp/CocktailAdapter.kt
@@ -1,41 +1,33 @@
 package com.example.cocktailapp
 
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cocktailapp.databinding.ItemCocktailBinding
 import com.squareup.picasso.Picasso
-
 
 class CocktailAdapter(var items: Array<Cocktail>) : RecyclerView.Adapter<CocktailAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(private val binding: ItemCocktailBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bindCocktailItem(item: Cocktail){
-            itemView.findViewById<CardView>(R.id.cardView).setOnClickListener {
-                Log.i(ViewHolder::class.simpleName, item.name.toString())
-                val navController = findNavController(itemView)
+            binding.cardView.setOnClickListener {
+                val navController = findNavController(binding.root)
                 val bundle = bundleOf("cocktail_id" to item.id)
                 navController.navigate(R.id.action_navigation_home_to_detail_cocktail, bundle)
             }
 
-            val imgView = itemView.findViewById<ImageView>(R.id.item_image)
-            Picasso.get().load(item.img).fit().into(imgView)
-            itemView.findViewById<TextView>(R.id.item_title).text = item.name
-            itemView.findViewById<TextView>(R.id.item_id).text = item.id.toString()
+            Picasso.get().load(item.img).fit().into(binding.itemImage)
+            binding.itemTitle.text = item.name
+            binding.itemId.text = item.id.toString()
         }
-
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): CocktailAdapter.ViewHolder {
-        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_cocktail, viewGroup, false)
-        return ViewHolder(v)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemCocktailBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
@@ -45,5 +37,4 @@ class CocktailAdapter(var items: Array<Cocktail>) : RecyclerView.Adapter<Cocktai
     override fun getItemCount(): Int {
         return items.size
     }
-
 }
--- app/src/main/java/com/example/cocktailapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/cocktailapp/ui/detail/DetailFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.room.Room
 import com.example.cocktailapp.*
 import com.example.cocktailapp.databinding.FragmentDetailBinding
+import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
@@ -42,35 +44,63 @@ class DetailFragment : Fragment(){
                     val cocktails = response.body().cocktails
                     if (cocktails.isNotEmpty() && cocktails.size == 1) {
                         Log.i(DetailFragment::class.simpleName, cocktails[0].name.toString())
+                        //val hashMap: MutableMap<String, String> = Gson().fromJson(cocktails[0], HashMap::class.java)
                         binding.cocktailTitle.text = cocktails[0].name
                         binding.glassTitle.text = cocktails[0].glass
                         binding.drinkCategory.text = cocktails[0].category
-                        if (cocktails[0].IBA.isNullOrEmpty()) {
-                            binding.drinkIBA.visibility = View.INVISIBLE
-                        } else {
-                            binding.drinkIBA.visibility = View.VISIBLE
-                            binding.drinkIBA.text = cocktails[0].IBA
-                        }
-                        val imgView = binding.cocktailImg
-                        Picasso.get().load(cocktails[0].img).fit().into(imgView)
-                        val tags = cocktails[0].tags.toString()
-                        val arrayTags = tags.split(",")
-                        mockData(cocktails[0])
+                        binding.drinkIBA.visibility = if (cocktails[0].IBA.isNullOrEmpty()) View.INVISIBLE else View.VISIBLE
+                        binding.drinkIBA.text = cocktails[0].IBA
+                        Picasso.get().load(cocktails[0].img).fit().into(binding.cocktailImg)
+                        val list: MutableList<String> = mockData(cocktails[0])
+                        //val arrayTags = cocktails[0].tags.toString().split(",")
                     }
                 }
             }
+
             override fun onFailure(call: Call<CocktailList>, t: Throwable) {
                 error(t.message.toString())
             }
         })
-
         return binding.root
     }
 
-    fun mockData(item: Cocktail) {
-        val list: MutableList<String>
+    fun mockData(item: Cocktail): MutableList<String>  {
+        val list: MutableList<String> = mutableListOf()
         if (!item.ingredient1.isNullOrEmpty()) {
             list.add(item.ingredient1.toString())
-        }
+        } else return list
+        if (!item.ingredient2.isNullOrEmpty()) {
+            list.add(item.ingredient2.toString())
+        } else return list
+        if (!item.ingredient3.isNullOrEmpty()) {
+            list.add(item.ingredient3.toString())
+        } else return list
+        if (!item.ingredient4.isNullOrEmpty()) {
+            list.add(item.ingredient4.toString())
+        } else return list
+        if (!item.ingredient5.isNullOrEmpty()) {
+            list.add(item.ingredient5.toString())
+        } else return list
+        if (!item.ingredient2.isNullOrEmpty()) {
+            list.add(item.ingredient2.toString())
+        } else return list
+        if (!item.ingredient2.isNullOrEmpty()) {
+            list.add(item.ingredient2.toString())
+        } else return list
+        if (!item.ingredient2.isNullOrEmpty()) {
+            list.add(item.ingredient2.toString())
+        } else return list
+        if (!item.ingredient2.isNullOrEmpty()) {
+            list.add(item.ingredient2.toString())
+        } else return list
+        if (!item.ingredient2.isNullOrEmpty()) {
+            list.add(item.ingredient2.toString())
+        } else return list
+
+
+
+        Log.i(DetailFragment::class.simpleName, list[1])
+
+        return list
     }
 }
