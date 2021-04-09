d9700f084f53a8f6f40c87666e96c016156c1929
Bunsarak PEN <bunsarak.pen@ynov.com>
2021-04-09 13:33:12 +0200

    CocktailApp Dashboard :
    Done :
    - Image of ingredient page fragment
    - Image of Dashboard view
    - Get number of ingredients to display in dashboard ingredients card

--- app/src/main/java/com/example/cocktailapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/cocktailapp/ui/dashboard/DashboardFragment.kt
@@ -9,9 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.cocktailapp.CocktailList
-import com.example.cocktailapp.CocktailListService
-import com.example.cocktailapp.R
+import com.example.cocktailapp.*
 import com.example.cocktailapp.databinding.FragmentDashboardBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -27,6 +25,7 @@ class DashboardFragment : Fragment() {
     private lateinit var dashboardViewModel: DashboardViewModel
     private lateinit var binding: FragmentDashboardBinding
     private var cocktailNumber = 0
+    private var ingredientsNumber = 0
 
     var fragment: Fragment? = null
     override fun onCreateView(
@@ -73,6 +72,31 @@ class DashboardFragment : Fragment() {
         })
     }
 
+    fun getDataIngredient() {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.thecocktaildb.com/api/json/v1/1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+
+        val service = retrofit.create(IngredientsService::class.java)
+        val call = service.getIngredientsData()
+
+        call.enqueue(object : Callback<IngredientsList> {
+            override fun onResponse(call: Call<IngredientsList>, response: Response<IngredientsList>) {
+                if (response.code() == 200) {
+                    ingredientsNumber = response.body().ingredients.size
+                    binding.ingredientsCardDashboard.text =  ingredientsNumber.toString() + " " + getString(R.string.Ingredients)
+                }
+            }
+
+            override fun onFailure(call: Call<IngredientsList>, t: Throwable) {
+                error(t.message.toString())
+            }
+        })
+    }
+
+
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -87,5 +111,6 @@ class DashboardFragment : Fragment() {
             navController.navigate(R.id.action_navigation_dashboard_to_ingredients_fragment)
         }
         getData()
+        getDataIngredient()
     }
 }
