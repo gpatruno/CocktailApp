d80571c9b440a5c0448088610257b0a409eea7b4
Bunsarak PEN <bunsarak.pen@ynov.com>
2021-04-09 13:16:16 +0200

    CocktailApp Dashboard :
    Done :
    -Ingredient Detail Fragment :
    - Scrollview for ingredient description
    - Icon ingredient contain alcohol or not
    - Alcool Level of Ingredient
    
    Ingredient Fragment :
    - Center image
    
    WIP :
    - Ingredient detail page design
    - Dashboard modification
    - Display the cocktail which use the ingredient

--- app/src/main/java/com/example/cocktailapp/Ingredients.kt
+++ app/src/main/java/com/example/cocktailapp/Ingredients.kt
@@ -3,6 +3,7 @@ package com.example.cocktailapp
 import com.google.gson.annotations.SerializedName
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 data class IngredientsList(
@@ -17,14 +18,47 @@ data class Ingredients(
 
     ) {}
 
+data class IngredientsBySearchList(
+        @SerializedName("ingredients")
+        val IngredientSearch: List<IngredientSearch>
+)
+
+data class IngredientSearch(
+        @SerializedName("idIngredient")
+        var idIngredient: Int? = null,
+
+        @SerializedName("strDescription")
+        var ingredientDescription: String? = null,
+
+        @SerializedName("strIngredient")
+        var strIngredient: String? = null,
+
+        @SerializedName("strType")
+        var strType: String? = null,
+
+        @SerializedName("strAlcohol")
+        var strAlcohol: String? = null,
+
+        @SerializedName("strABV")
+        var strABV: Int? = null,
+
+) {}
 
 
+
+
+// List Ingredients : www.thecocktaildb.com/api/json/v1/1/list.php?i=list
 interface IngredientsService {
     @GET("list.php")
     fun getIngredientsData(@Query("i") c: String= "list"): Call<IngredientsList>
 }
 
-interface IngredientsImagesService {
-    @GET("list.php")
-    fun getIngredientsImageData(@Query("i") c: String= "list"): Call<IngredientsList>
+//Search ingredient by name
+// example : www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka
+interface IngredientsSearchService {
+
+    abstract val ingredientDetail: String
+
+    @GET("search.php")
+    fun getIngredientsSearch(@Query("i") c: String = ingredientDetail): Call<IngredientsBySearchList>
 }
--- app/src/main/java/com/example/cocktailapp/ui/ingredients/IngredientDetailFragment.kt
+++ app/src/main/java/com/example/cocktailapp/ui/ingredients/IngredientDetailFragment.kt
@@ -1,23 +1,73 @@
 package com.example.cocktailapp.ui.ingredients
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.cocktailapp.R
+import com.example.cocktailapp.*
 import com.example.cocktailapp.databinding.FragmentIngredientDetailBinding
 import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class IngredientDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentIngredientDetailBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentIngredientDetailBinding.inflate(layoutInflater)
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.thecocktaildb.com/api/json/v1/1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val service = retrofit.create(IngredientsSearchService::class.java)
+        val call = arguments?.getString("ingredient_name")?.let { service.getIngredientsSearch(it) }
+
+        call?.enqueue(object : Callback<IngredientsBySearchList> {
+            @SuppressLint("SetTextI18n")
+            override fun onResponse(call: Call<IngredientsBySearchList>, response: Response<IngredientsBySearchList>) {
+                if (response.code() == 200) {
+                    val detailIngredient = response.body().IngredientSearch
+                    val descriptionIngredient = detailIngredient[0].ingredientDescription
+                    val ingredientAlcohol = detailIngredient[0].strAlcohol
+                    val ingredientAlcoholPurcentage = detailIngredient[0].strABV
+                    if (descriptionIngredient !== null) {
+                        binding.ingredientDescription.text = descriptionIngredient.toString()
+                    } else {
+                        binding.ingredientDescription.text = "Aucune description disponible"
+                    }
+
+                    if (ingredientAlcohol !== null) {
+                        binding.ingredientAlcool.setImageResource(R.drawable.ic_alcool)
+                    } else {
+                        binding.ingredientAlcool.setImageResource(R.drawable.ic_pas_d_alcool)
+                    }
+
+                    if(ingredientAlcoholPurcentage !== null) {
+                        binding.purcentageAlcool.text= "$ingredientAlcoholPurcentage°"
+                    } else {
+                        binding.purcentageAlcool.text= "0°"
+                    }
+
+
+                }
+            }
+
+            override fun onFailure(call: Call<IngredientsBySearchList>, t: Throwable) {
+                error(t.message.toString())
+            }
+        })
 
         return binding.root
     }
@@ -27,10 +77,13 @@ class IngredientDetailFragment : Fragment() {
         val ingredientName = arguments?.getString("ingredient_name")
         binding.ingredientsName.text = ingredientName
         val imgIngredient = binding.itemImageIngredientDetail
-        Picasso.get().load("https://www.thecocktaildb.com/images/ingredients/$ingredientName.png").fit().into(imgIngredient)
+        Picasso.get().load("https://www.thecocktaildb.com/images/ingredients/$ingredientName-Medium.png").fit().into(imgIngredient)
 
 
     }
 
 
 }
+
+
+
